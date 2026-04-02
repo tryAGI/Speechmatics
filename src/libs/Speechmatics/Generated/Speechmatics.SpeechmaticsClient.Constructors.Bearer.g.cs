473c5d0b9e461562364cbdba6de9baf343e821b0
Konstantin S. <havendv@gmail.com>
2026-04-02 11:06:56 +0400

    feat: Updated OpenAPI spec (#7)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Speechmatics/Generated/Speechmatics.SpeechmaticsClient.Constructors.Bearer.g.cs
+++ src/libs/Speechmatics/Generated/Speechmatics.SpeechmaticsClient.Constructors.Bearer.g.cs
@@ -15,7 +15,7 @@ namespace Speechmatics
         {
             Authorizing(HttpClient, ref apiKey);
 
-            AuthorizeUsingApiKeyInHeader(apiKey);
+            AuthorizeUsingBearer(apiKey);
 
             Authorized(HttpClient);
         }
