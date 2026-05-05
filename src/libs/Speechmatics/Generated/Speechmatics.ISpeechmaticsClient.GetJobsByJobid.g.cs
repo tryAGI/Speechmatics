1e3f4958933dcc9b7fdb9f969e7116db8bd626f6
Konstantin S. <havendv@gmail.com>
2026-05-05 17:31:01 +0400

    feat: Updated OpenAPI spec (#19)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Speechmatics/Generated/Speechmatics.ISpeechmaticsClient.GetJobsByJobid.g.cs
+++ src/libs/Speechmatics/Generated/Speechmatics.ISpeechmaticsClient.GetJobsByJobid.g.cs
@@ -16,5 +16,17 @@ namespace Speechmatics
             string jobid,
             global::Speechmatics.AutoSDKRequestOptions? requestOptions = default,
             global::System.Threading.CancellationToken cancellationToken = default);
+        /// <summary>
+        /// Get job details<br/>
+        /// Get job details, including progress and any error reports.
+        /// </summary>
+        /// <param name="jobid"></param>
+        /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
+        /// <param name="cancellationToken">The token to cancel the operation with</param>
+        /// <exception cref="global::Speechmatics.ApiException"></exception>
+        global::System.Threading.Tasks.Task<global::Speechmatics.AutoSDKHttpResponse<global::Speechmatics.RetrieveJobResponse>> GetJobsByJobidAsResponseAsync(
+            string jobid,
+            global::Speechmatics.AutoSDKRequestOptions? requestOptions = default,
+            global::System.Threading.CancellationToken cancellationToken = default);
     }
 }
