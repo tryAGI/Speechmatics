c0395ec2c10ce73fc6efe273c35c42efde278d4c
Konstantin S. <havendv@gmail.com>
2026-08-14 19:46:04 +0400

    feat: Updated OpenAPI spec (#40)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Speechmatics/Generated/Speechmatics.SpeechmaticsClient.g.cs
+++ src/libs/Speechmatics/Generated/Speechmatics.SpeechmaticsClient.g.cs
@@ -34,10 +34,17 @@ namespace Speechmatics
 
         /// <inheritdoc/>
         public global::Speechmatics.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Speechmatics.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Speechmatics.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
